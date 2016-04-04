d781d0fc0d1baef5432c087c187cd38d1a9532fd
Yari Mattei <yari.mattei@gmail.com>
2016-04-04 21:25:46 +0200

    change welcome print

--- src/main/java/ch/yarox/invoiceManager/Main.java
+++ src/main/java/ch/yarox/invoiceManager/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome !!!");
+		System.out.println("Invoice Manager !");
 //		Person p = new Customer("name", "surname");
 //		System.out.println("hello");
 		
